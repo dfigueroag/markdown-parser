28585780a51b05c00312499fdc7abeff254555f0
ima-quack <103283986+ima-quack@users.noreply.github.com>
2022-04-29 14:07:09 -0700

    Updated the Markdown Parser to accomodate for the 8 test cases

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,18 +10,55 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
+        int newLinkIndex = 0;
+        while(newLinkIndex < markdown.length()) {
+            int openBracket = markdown.indexOf("[", newLinkIndex);
+            
+            int indexBeforeOpenBracket = openBracket - 1;
+
+            if(openBracket < 0){
+                break;
+            }
+
             int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", currentIndex);
+
+            if (closeBracket == openBracket + 1) {
+                newLinkIndex = closeBracket + 1;
+                continue;
+            }
+
+            if(closeBracket < 0){
+                break;
+            }
+
+            int openParen = markdown.indexOf("(", closeBracket);
+
+            if(openParen < 0){
+                break;
+            }
+
+            if(openParen != closeBracket+1){
+                newLinkIndex = closeBracket;
+                continue;
+            }
+
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            
+            if(closeParen < 0){
+                break;
+            }
+
+            if (indexBeforeOpenBracket > 0  && markdown.charAt(indexBeforeOpenBracket) != '!') {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+
+            newLinkIndex = closeParen + 1;
+
         }
 
         return toReturn;
     }
+
 
 
     public static void main(String[] args) throws IOException {
